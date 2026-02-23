cd011e6da8a12154c603e7e7eabf7cb375a8cd9a
Sahood CV <sahood@appsfinal.com>
2026-02-23 13:28:08 +0530

    add icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -33,7 +34,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head></head>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;600;700;800;900&family=Inter:wght@300;400;500;600&display=swap"
+          rel="stylesheet"
+        />
+        <Script
+          src="http://localhost:3000/sdk.js"
+          data-key="es_c20dd9329dc6c14453de8e34d225957753bd74edda1759dc87914c76941ec64a"
+          strategy="afterInteractive"
+        />
+      </head>
       <body className="bg-cosmic-black text-white antialiased">
         {children}
       </body>
