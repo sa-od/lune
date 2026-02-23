0b759650bc271f07415cc23c7a1bdcaab21b07e2
Sahood CV <sahood@appsfinal.com>
2026-02-23 12:29:41 +0530

    add event spy

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,18 +33,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;600;700;800;900&family=Inter:wght@300;400;500;600&display=swap"
-          rel="stylesheet"
-        />
-      </head>
+      <head></head>
       <body className="bg-cosmic-black text-white antialiased">
         {children}
       </body>
